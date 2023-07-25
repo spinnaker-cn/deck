0d0b0ab9165963ce6eba0c816061da963f07e6f0
zhangjin <56920257@qq.com>
2023-07-25 21:56:48 +0800

    完成pipeline-deploy子网信息展示

--- app/scripts/modules/ctyun/src/subnet/subnet.renderer.ts
+++ app/scripts/modules/ctyun/src/subnet/subnet.renderer.ts
@@ -1,10 +1,41 @@
-import { module } from 'angular';
-
-import { IServerGroup } from '@spinnaker/core';
+import { module, IPromise } from 'angular';
+import { IServerGroup, SubnetReader } from '@spinnaker/core';
 
 export class AwsSubnetRenderer {
+  private cache: any = [];
+  constructor() {
+    SubnetReader.listSubnetsByProvider('ctyun').then(subnet => {
+      this.cache = subnet;
+    });
+  }
+
   public render(serverGroup: IServerGroup): string {
-    return serverGroup.subnetType;
+    let str = '';
+    if (serverGroup.mazInfoList) {
+      serverGroup.mazInfoList.map(maz => {
+        if (str != '') {
+          str += ',';
+        }
+        if (maz.masterId) {
+          if (this.cache) {
+            str += this.cache.find(sub => sub.id == maz.masterId).name || '';
+          } else {
+            str += maz.masterId;
+          }
+        }
+        if (str != '') {
+          str += ',';
+        }
+        if (maz.optionId) {
+          if (this.cache) {
+            str += this.cache.find(sub => sub.id == maz.optionId).name || '';
+          } else {
+            str += maz.optionId;
+          }
+        }
+      });
+    }
+    return str;
   }
 }
 
